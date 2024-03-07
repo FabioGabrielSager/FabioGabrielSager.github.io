f0335e574fb86ef077ce0aabe5c70d5f0e640794
shackgerc <sagerprogramer@gmail.com>
2024-03-06 21:40:23 -0300

    Optimizing welcome section image

--- src/app/components/welcome/welcome.component.ts
+++ src/app/components/welcome/welcome.component.ts
@@ -1,10 +1,17 @@
 import {Component} from '@angular/core';
+import {NgOptimizedImage, provideCloudinaryLoader} from "@angular/common";
 
 @Component({
-    selector: 'app-welcome',
-    templateUrl: './welcome.component.html',
-    styleUrls: ['./welcome.component.css'],
-    standalone: true
+  selector: 'app-welcome',
+  templateUrl: './welcome.component.html',
+  styleUrls: ['./welcome.component.css'],
+  imports: [
+    NgOptimizedImage
+  ],
+  standalone: true,
+  providers: [
+    provideCloudinaryLoader('https://res.cloudinary.com/dqydqgfyk')
+  ]
 })
 export class WelcomeComponent {
 
