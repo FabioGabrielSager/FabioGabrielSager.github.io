4f0a90e5238c5612a600efb9eb23d0175aa6b97a
shackgerc <sagerprogramer@gmail.com>
2024-03-08 21:44:35 -0300

    Adding Inventory API to projects

--- src/app/components/portfolio-section/porfolio/portfolio.component.ts
+++ src/app/components/portfolio-section/porfolio/portfolio.component.ts
@@ -18,15 +18,27 @@ export class PortfolioComponent {
 
   constructor() {
 
-    const SimplyChesscard: CardData = {
+    const SimplyChessCard: CardData = {
       title: "Simply Chess",
       description: "Plataforma multijugador de ajedrez en tiempo real que ofrece partidas privadas para competir " +
         "de manera segura, junto con una cola de búsqueda de partidas para una experiencia fluida y sin interrupciones.",
       sourceCodeLink: "https://github.com/shackGerc/Simply-Chess",
-      createdWith: ["Spring Boot", "Angular", "Bootstrap", "SockJS", "Stomp", "WebSocket", "Postgresql"],
+      createdWith: ["Spring Boot", "Angular", "Bootstrap", "SockJS", "Stomp", "WebSocket", "PostgreSQL"],
       imageSrc: "assets/projects/SimplyChess.webp"
     } as CardData
 
-    this.cards.push(SimplyChesscard);
+    const InventoryApiCard: CardData = {
+      title: "Inventory API",
+      description: "En el marco del Trabajo Práctico Integrador (TPI) de la Tecnicatura Universitaria en Programación " +
+        "de la UTN-FRC en 2023, colaboramos en la elaboración de una aplicación para la administración de una ferretería " +
+        "basada en microservicios. Nuestro grupo se encargo del diseño y desarrollo del módulo de inventario. " +
+        "Este es el resultado de nuestro trabajo.",
+      sourceCodeLink: "https://github.com/shackGerc/Inventory-API",
+      createdWith: ["Spring Boot", "PostgreSQL"],
+      imageSrc: "assets/projects/inventory.webp"
+    } as CardData
+
+    this.cards.push(SimplyChessCard);
+    this.cards.push(InventoryApiCard)
   }
 }
